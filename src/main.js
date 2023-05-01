8071de57244f22c4985a8cbc5f1be3589460b184
lyq <705194673@qq.com>
2023-05-01 15:44:07 +0800

    feat: 引入全局样式

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createSSRApp } from 'vue';
 import App from './App.vue';
+import './css/font.css';
+import './css/theme.css';
 
 export function createApp() {
     const app = createSSRApp(App);
