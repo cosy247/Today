131a0e1aa9c3829c86582f69a40461b56aa45766
lyq <705194673@qq.com>
2023-05-07 02:03:17 +0800

    ci: 将全局css写入app.vue中

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,6 @@
+<template>
+    <p class="kkkk">123</p>
+</template>
 <script>
     export default {
         onLaunch: function () {
@@ -11,3 +14,43 @@
         },
     };
 </script>
+
+<style>
+    /*********************** font ***********************/
+    @font-face {
+        font-family: 'iconfont'; /* Project id 4045984 */
+        src: url('//at.alicdn.com/t/c/font_4045984_1b2afz26zzz.woff2?t=1682954519206') format('woff2'), url('//at.alicdn.com/t/c/font_4045984_1b2afz26zzz.woff?t=1682954519206') format('woff'), url('//at.alicdn.com/t/c/font_4045984_1b2afz26zzz.ttf?t=1682954519206') format('truetype');
+    }
+
+    @font-face {
+        font-family: 'app-font'; /* Project id 4045984 */
+        src: url('../static/font/font.ttf') format('truetype');
+    }
+
+    * {
+        padding: 0;
+        margin: 0;
+        font-family: iconfont;
+    }
+
+    .font {
+        font-family: app-font, iconfont;
+    }
+
+    span {
+        font-family: inherit;
+    }
+
+    :root {
+        --fontSize-ss: 28rpx;
+        --fontSize-s: 35rpx;
+        --fontSize-m: 50rpx;
+        --fontSize-l: 65rpx;
+    }
+
+    /*********************** background ***********************/
+    .background {
+        background: -webkit-linear-gradient(top, transparent 40rpx, rgb(218, 218, 106) 41rpx), -webkit-linear-gradient(left, rgb(241, 245, 246) 40rpx, rgb(218, 218, 106) 41rpx);
+        background-size: 42rpx 42rpx;
+    }
+</style>
