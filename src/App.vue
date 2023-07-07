4a356bc4f86ce433fcd8aa4e6c4615add2377a53
yqli <705194673@qq.com>
2023-07-07 10:08:02 +0800

    🐎ci: 添加字体

--- src/App.vue
+++ src/App.vue
@@ -18,23 +18,17 @@
         font-family: 'iconfont'; /* Project id 4045984 */
         src: url('//at.alicdn.com/t/c/font_4045984_ntmualuv72.woff2?t=1686274231478') format('woff2'), url('//at.alicdn.com/t/c/font_4045984_ntmualuv72.woff?t=1686274231478') format('woff'), url('//at.alicdn.com/t/c/font_4045984_ntmualuv72.ttf?t=1686274231478') format('truetype');
     }
-    /* @font-face {
-        font-family: 'app-font';
-        src: url('../static/font/font.ttf') format('truetype');
-    } */
+
+    /* 在线链接服务仅供平台体验和调试使用，平台不承诺服务的稳定性，企业客户需下载字体包自行发布使用并做好备份。 */
+    @font-face {
+        font-family: 'app-font'; /* Project id 4045984 */
+        src: url('../static/font/HarmonyOS_Sans_SC_Regular.ttf')  format('truetype');
+    }
 
     * {
         padding: 0;
         margin: 0;
-        font-family: iconfont;
-    }
-
-    .font {
-        font-family: app-font, iconfont;
-    }
-
-    span {
-        font-family: inherit;
+        font-family: iconfont, app-font ;
     }
 
     :root {
@@ -44,6 +38,6 @@
     }
 
     body {
-        background: #f6f7f8;
+        background: #fefefe;
     }
 </style>
